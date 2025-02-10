e458aded90c94503f3946fb6db7068a6eae54d80
Stoyan Minchev <154098583+StoyanMinc@users.noreply.github.com>
2025-02-11 00:31:16 +0200

    Fix home page css

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component.js';
+import { DocumentsComponent } from './documents/documents.component.js';
 
 export const routes: Routes = [
     { path:'', redirectTo:'home', pathMatch:"full" },
-    { path:'home', component:HomeComponent }
+    { path:'home', component:HomeComponent },
+    {path: 'documents',component:DocumentsComponent }
 ];
